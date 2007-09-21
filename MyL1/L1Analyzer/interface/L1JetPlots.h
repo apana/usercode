52425d2809e073e4d0e4999f3c683251c09a6e40
apana <>
2007-09-21 16:53:30 +0000

    Updated code

--- MyL1/L1Analyzer/interface/L1JetPlots.h
+++ MyL1/L1Analyzer/interface/L1JetPlots.h
@@ -1,6 +1,5 @@
 #ifndef MyL1_L1Analyzer_L1JetPlots_h
 #define MyL1_L1Analyzer_L1JetPlots_h
-#include <TH1.h>
 /* \class L1JetPlots
  *
  * \author Leonard Apanasevich
@@ -9,6 +8,34 @@
  *
  */
 #include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "MyL1/L1Analyzer/interface/L1JetPlots.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+
+#include "DataFormats/L1Trigger/interface/L1JetParticle.h"
+#include "DataFormats/L1Trigger/interface/L1JetParticleFwd.h"
+
+#include "DataFormats/Math/interface/deltaR.h"
+
+#include <TROOT.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TSystem.h>
+#include <TFile.h>
+#include <TCanvas.h>
+#include <cmath>
+#include <map>
+
+using namespace edm;
+using namespace reco;
+using namespace std;
 
 class L1JetPlots : public edm::EDAnalyzer {
 public:
@@ -18,17 +45,39 @@ private:
   void beginJob( const edm::EventSetup & );
   void analyze( const edm::Event& , const edm::EventSetup& );
   void endJob();
-  std::string CaloJetAlgorithm, GenJetAlgorithm, recmet_,genmet_, calotowers_, histogram;
 
-  double globalThreshold;
+  void L1Analysis( const CaloJetCollection&,
+		   const GenJetCollection&,
+		   const l1extra::L1JetParticleCollection&);
 
-  TH1F h_ptCal, h_etaCal, h_phiCal;
-  TH1F h_ptGen, h_etaGen, h_phiGen;
+  template <typename T> void mtchL1(const Double_t&, const Double_t&, const Double_t&, 
+				    const T& jets, const TString&);
 
-  TH1F h_MetPt,h_genMetPt,h_MetPt_cT,h_MetPt_cT_be,h_MetPt_cT_f;
+  std::string CaloJetAlgorithm, GenJetAlgorithm, recmet_,genmet_, histogram;
+  edm::InputTag l1CollectionsTag_;
+  int errCnt;
+  const int errMax(){return 100;}
 
+  void fillHist(const TString& histName, const Double_t& value, const Double_t& wt=1.0);
+  void fill2DHist(const TString& histName, const Double_t& x,const Double_t& y,const Double_t& wt=1.0);
+
+  bool doCaloJets,doGenJets,doCaloMET,doGenMET;
+  bool doL1Jets;
+
+  TH1F ptCal, etaCal, phiCal;
+  TH1F ptGen, etaGen, phiGen;
+
+  TH1F MetPt, genMetPt;
 
   TFile* m_file;
+
+  // use the map function to access the rest of the histograms
+  std::map<TString, TH1*> m_HistNames;
+  std::map<TString, TH1*>::iterator hid;
+
+  std::map<TString, TH2*> m_HistNames2D;
+  std::map<TString, TH2*>::iterator hid2D;
+
 };
 
 #endif
--- MyL1/L1Analyzer/src/L1JetPlots.cc
+++ MyL1/L1Analyzer/src/L1JetPlots.cc
@@ -3,173 +3,269 @@
 // Author: Robert M. Harris
 // Date:  28 - August - 2006
 //
-#include "MyL1/L1JetPlots/JetPlotsExample.h"
-#include "DataFormats/JetReco/interface/CaloJetCollection.h"
-#include "DataFormats/JetReco/interface/CaloJet.h"
-#include "DataFormats/JetReco/interface/GenJet.h"
-#include "DataFormats/METReco/interface/CaloMETCollection.h"
-#include "DataFormats/METReco/interface/GenMETCollection.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include <TROOT.h>
-#include <TSystem.h>
-#include <TFile.h>
-#include <TCanvas.h>
-#include <cmath>
-using namespace edm;
-using namespace reco;
-using namespace std;
+
+#include "MyL1/L1Analyzer/interface/L1JetPlots.h"
 
 // Get the algorithm of the jet collections we will read from the .cfg file
 // which defines the value of the strings CaloJetAlgorithm and GenJetAlgorithm.
-JetPlotsExample::JetPlotsExample( const ParameterSet & cfg ) :
-  CaloJetAlgorithm( cfg.getParameter<string>( "CaloJetAlgorithm" ) ),
-  GenJetAlgorithm( cfg.getParameter<string>( "GenJetAlgorithm" ) ),
-  recmet_(cfg.getParameter< string > ("recmet")),
-  genmet_(cfg.getParameter< string > ("genmet")),
-  calotowers_(cfg.getParameter< string > ("calotowers")),
-  histogram( cfg.getParameter<string>( "Histogram" ) )
-  {
-    globalThreshold=0.;
+L1JetPlots::L1JetPlots( const ParameterSet & cfg ) {
+  cout << " Beginning L1Jet Analysis " << endl;
+  CaloJetAlgorithm = cfg.getParameter<string>( "CaloJetAlgorithm" );
+  GenJetAlgorithm  = cfg.getParameter<string>( "GenJetAlgorithm" );
+  recmet_          = cfg.getParameter< string > ("recmet");
+  genmet_          = cfg.getParameter< string > ("genmet");
+  l1CollectionsTag_= cfg.getParameter< InputTag > ("l1collections");
+  histogram        = cfg.getParameter<string>( "Histogram" );
+
+  errCnt=0;
 }
 
-void JetPlotsExample::beginJob( const EventSetup & ) {
+void L1JetPlots::beginJob( const EventSetup & ) {
 
   // Open the histogram file and book some associated histograms
   m_file=new TFile(histogram.c_str(),"RECREATE");
-  h_ptCal =  TH1F( "ptCal",  "p_{T} of leading CaloJets", 50, 0, 1000 );
-  h_etaCal = TH1F( "etaCal", "#eta of leading CaloJets", 50, -3, 3 );
-  h_phiCal = TH1F( "phiCal", "#phi of leading CaloJets", 50, -M_PI, M_PI );
-  h_ptGen =  TH1F( "ptGen",  "p_{T} of leading GenJets", 50, 0, 1000 );
-  h_etaGen = TH1F( "etaGen", "#eta of leading GenJets", 50, -3, 3 );
-  h_phiGen = TH1F( "phiGen", "#phi of leading GenJets", 50, -M_PI, M_PI );
+  ptCal =  TH1F( "ptCal",  "p_{T} of leading CaloJets", 50, 0, 1000 );
+  etaCal = TH1F( "etaCal", "#eta of leading CaloJets", 50, -3, 3 );
+  phiCal = TH1F( "phiCal", "#phi of leading CaloJets", 50, -M_PI, M_PI );
+  ptGen =  TH1F( "ptGen",  "p_{T} of leading GenJets", 50, 0, 1000 );
+  etaGen = TH1F( "etaGen", "#eta of leading GenJets", 50, -3, 3 );
+  phiGen = TH1F( "phiGen", "#phi of leading GenJets", 50, -M_PI, M_PI );
 
   int nbins=50;
   Double_t min=0.,max=150.;
 
-  h_MetPt = TH1F( "h_MetPt", "Reconstructed MET ", nbins, min, max );
-  h_genMetPt = TH1F( "h_genMetPt", "Generated MET ", nbins, min, max );
+  MetPt = TH1F( "MetPt", "Reconstructed MET ", nbins, min, max );
+  genMetPt = TH1F( "genMetPt", "Generated MET ", nbins, min, max );
 
-  h_MetPt_cT = TH1F( "h_MetPt_cT", "Reconstructed MET -- from CaloTowers", nbins, min, max );
-  h_MetPt_cT_be = TH1F( "h_MetPt_cT_be", "Reconstructed MET -- from CaloTowers -- Barrel and Endcaps", nbins, min, max );
-  h_MetPt_cT_f = TH1F( "h_MetPt_cT_f", "Reconstructed MET -- from CaloTowers -- Forward", nbins, min, max );
+  TString hname,htitle;
+
+  hname="L1JetCollSize"; htitle="L1 Jet Collection Size";
+  m_HistNames[hname] = new TH1F( hname , htitle  , 10, -0.5, 9.5 );
+
+  Int_t nptbins=40;
+  Double_t ptmin=0,ptmax=200.;
+  hname="L1JetPt"; htitle="L1 Jet p_{T} -- Central";
+  m_HistNames[hname] = new TH1F( hname , htitle  , nptbins, ptmin, ptmax );
+  m_HistNames[hname]->Sumw2();
+
+  Int_t nphibins=20;
+  Double_t phimin=-M_PI,phimax=M_PI;
+  hname="L1JetPhi"; htitle="L1 Jet #phi -- Central";
+  m_HistNames[hname] = new TH1F( hname , htitle  , nphibins, phimin, phimax );
+  m_HistNames[hname]->Sumw2();
+
+  Int_t netabins=80;
+  Double_t etamin=-5,etamax=5.;
+  hname="L1JetEta"; htitle="L1 Jet #eta -- Central";
+  m_HistNames[hname] = new TH1F( hname , htitle  , netabins, etamin, etamax );  m_HistNames[hname]->Sumw2();
+
+  hname="L1DeltaR_Calo"; htitle="Delta R -- L1 and CaloJets";
+  m_HistNames[hname] = new TH1F( hname , htitle  , 100, 0., 10. );
+  m_HistNames[hname]->Sumw2();
+
+  hname="L1DeltaR_Gen"; htitle="Delta R -- L1 and GenJets";
+  m_HistNames[hname] = new TH1F( hname , htitle  , 100, 0., 10. );
+  m_HistNames[hname]->Sumw2();
+
+  hname="L1PtOverCaloPt_Calo";htitle=" L1 Jet p_{T} over CaloJet p_{T}";
+  m_HistNames[hname] = new TH1F( hname , htitle  , 40, 0., 2.0 );
+  m_HistNames[hname]->Sumw2();
+
+  hname="L1PtOverCaloPt_Gen";htitle=" L1 Jet p_{T} over GenJet p_{T}";
+  m_HistNames[hname] = new TH1F( hname , htitle  , 40, 0., 2.0 );
+  m_HistNames[hname]->Sumw2();
+
+  Int_t n2dbins=50;
+  hname="L1etaVSJeteta_Calo"; htitle="L1 Jet #eta vs CaloJet #eta -- Central";
+  m_HistNames2D[hname] = new TH2F( hname , htitle  , n2dbins, etamin, etamax, n2dbins, etamin, etamax );
+  hname="L1etaVSJeteta_Gen"; htitle="L1 Jet #eta vs GenJet #eta -- Central";
+  m_HistNames2D[hname] = new TH2F( hname , htitle  , n2dbins, etamin, etamax, n2dbins, etamin, etamax );
+
+
+  hname="L1phiVSJetphi_Calo"; htitle="L1 Jet #phi vs CaloJet #phi -- Central";
+  m_HistNames2D[hname] = new TH2F( hname , htitle  , n2dbins, phimin, phimax, n2dbins, phimin, phimax );
+  hname="L1phiVSJetphi_Gen"; htitle="L1 Jet #phi vs GenJet #phi -- Central";
+  m_HistNames2D[hname] = new TH2F( hname , htitle  , n2dbins, phimin, phimax, n2dbins, phimin, phimax );
+
+  hname="L1ptVSJetpt_Calo"; htitle="L1 Jet p_{T} vs CaloJet p_{T} -- Central";
+  m_HistNames2D[hname] = new TH2F( hname , htitle  , n2dbins, ptmin, ptmax, n2dbins, ptmin, ptmax );
+  hname="L1ptVSJetpt_Gen"; htitle="L1 Jet p_{T} vs GenJet p_{T} -- Central";
+  m_HistNames2D[hname] = new TH2F( hname , htitle  , n2dbins, ptmin, ptmax, n2dbins, ptmin, ptmax );
 
 }
 
-void JetPlotsExample::analyze( const Event& evt, const EventSetup& es ) {
-
-  //Get the CaloJet collection
-  Handle<CaloJetCollection> caloJets;
-  evt.getByLabel( CaloJetAlgorithm, caloJets );
-
-  //Loop over the two leading CaloJets and fill some histograms
-  int jetInd = 0;
-  for( CaloJetCollection::const_iterator cal = caloJets->begin(); cal != caloJets->end() && jetInd<2; ++ cal ) {
-    // std::cout << "CALO JET #" << jetInd << std::endl << cal->print() << std::endl;
-    h_ptCal.Fill( cal->pt() );
-    h_etaCal.Fill( cal->eta() );
-    h_phiCal.Fill( cal->phi() );
-    jetInd++;
-  }
-
-  //Get the GenJet collection
-  Handle<GenJetCollection> genJets;
-  evt.getByLabel( GenJetAlgorithm, genJets );
-
-  //Loop over the two leading GenJets and fill some histograms
-  jetInd = 0;
-  for( GenJetCollection::const_iterator gen = genJets->begin(); gen != genJets->end() && jetInd<2; ++ gen ) {
-    // std::cout << "GEN JET #" << jetInd << std::endl << gen->print() << std::endl;
-    h_ptGen.Fill( gen->pt() );
-    h_etaGen.Fill( gen->eta() );
-    h_phiGen.Fill( gen->phi() );
-    jetInd++;
-  }
-
-  Handle<CaloMETCollection> recmet;
-  evt.getByLabel (recmet_,recmet);
-  Handle<GenMETCollection>  genmet;
-  evt.getByLabel (genmet_,genmet);
-
-  for( CaloMETCollection::const_iterator met = recmet->begin(); met != recmet->end() ; ++ met ) {
-    h_MetPt.Fill(met->pt());
-  }
-
-  for( GenMETCollection::const_iterator met = genmet->begin(); met != genmet->end() ; ++ met ) {
-    h_genMetPt.Fill(met->pt());
-  }
+void L1JetPlots::analyze( const Event& evt, const EventSetup& es ) {
 
   string errMsg("");
-  try {
-    Handle<CaloTowerCollection> caloTowers;
-    evt.getByLabel (calotowers_,caloTowers);
+  doCaloJets=true; doGenJets=true; doCaloMET=true; doGenMET=true, doL1Jets=true;
 
-    double sum_et = 0.0;
-    double sum_ex = 0.0;
-    double sum_ey = 0.0;
+  //Get the collections
+  Handle<CaloJetCollection> caloJets;
+  Handle<GenJetCollection> genJets;
+  Handle<CaloMETCollection> recmet;
+  Handle<GenMETCollection>  genmet;
+  Handle<l1extra::L1JetParticleCollection> l1jets;
 
-    double sum_et_be = 0.0;
-    double sum_ex_be = 0.0;
-    double sum_ey_be = 0.0;
+  try { evt.getByLabel( CaloJetAlgorithm, caloJets );} catch (...) { errMsg=errMsg + "  -- No RecJets"; doCaloJets=false;}
+  try { evt.getByLabel( GenJetAlgorithm, genJets );} catch (...) { errMsg=errMsg + "  -- No GenJets"; doGenJets=false;}
+  try { evt.getByLabel( recmet_,recmet );} catch (...) { errMsg=errMsg + "  -- No RecMET"; doCaloMET=false;}
+  try { evt.getByLabel( genmet_,genmet );} catch (...) { errMsg=errMsg + "  -- No GenMET"; doGenMET=false;}
 
-    double sum_et_f = 0.0;
-    double sum_ex_f = 0.0;
-    double sum_ey_f = 0.0;
+  InputTag L1JetTag(edm::InputTag(l1CollectionsTag_.label(),"Central"));
+  try { evt.getByLabel(L1JetTag,l1jets); } catch (...) 
+    { errMsg=errMsg + "  -- No L1Jets with name: " + L1JetTag.label() ; doL1Jets=false;}
 
-    double sum_ez = 0.0;
-    for ( CaloTowerCollection::const_iterator tower=caloTowers->begin();
-	  tower!=caloTowers->end(); tower++) {
 
-      if( tower->et() > globalThreshold )
-	{
-	  double phi   = tower->phi();
-	  //double theta = tower->theta();
-	  double e     = tower->energy();
-	  double et    = tower->et();
-	  double eta   = tower->eta();
-
-	  //sum_ez += e*cos(theta);
-	  sum_et += et;
-	  sum_ex += et*cos(phi);
-	  sum_ey += et*sin(phi);
-
-	  if (fabs(eta) < 2.5){
-	    sum_et_be += et;
-	    sum_ex_be += et*cos(phi);
-	    sum_ey_be += et*sin(phi);
-	  }else{
-	    sum_et_f += et;
-	    sum_ex_f += et*cos(phi);
-	    sum_ey_f += et*sin(phi);
-	  }
-	}
+  if (doCaloJets){
+    //Loop over the two leading CaloJets and fill some histograms
+    int jetInd = 0;
+    for( CaloJetCollection::const_iterator cal = caloJets->begin(); cal != caloJets->end() && jetInd<2; ++ cal ) {
+      // std::cout << "CALO JET #" << jetInd << std::endl << cal->print() << std::endl;
+      ptCal.Fill( cal->pt() );
+      etaCal.Fill( cal->eta() );
+      phiCal.Fill( cal->phi() );
+      jetInd++;
     }
-    double mex   = -sum_ex;
-    double mey   = -sum_ey;
-    //double mez   = -sum_ez;
-    double met   = sqrt( sum_ex*sum_ex + sum_ey*sum_ey );
-    double met_be   = sqrt( sum_ex_be*sum_ex_be + sum_ey_be*sum_ey_be );
-    double met_f   = sqrt( sum_ex_f*sum_ex_f + sum_ey_be*sum_ey_f );
+  }
 
-    double sumet = sum_et;
-    double phi   = atan2( -sum_ey, -sum_ex );
+  if (doGenJets){
+    //Loop over the two leading GenJets and fill some histograms
+    int jetInd = 0;
+    for( GenJetCollection::const_iterator gen = genJets->begin(); gen != genJets->end() && jetInd<2; ++ gen ) {
+      // std::cout << "GEN JET #" << jetInd << std::endl << gen->print() << std::endl;
+      ptGen.Fill( gen->pt() );
+      etaGen.Fill( gen->eta() );
+      phiGen.Fill( gen->phi() );
+      jetInd++;
+    }
+  }
 
-    h_MetPt_cT.Fill(met);
-    h_MetPt_cT_be.Fill(met_be);
-    h_MetPt_cT_f.Fill(met_f);
+  if (doCaloMET){
+    for( CaloMETCollection::const_iterator met = recmet->begin(); met != recmet->end() ; ++ met ) {
+      MetPt.Fill(met->pt());
+    }
+  }
 
-  } catch (const cms::Exception& e){
-    errMsg=errMsg + "  -- No CaloTowers\n"+e.what();
-    std::cout << errMsg << std::endl;
+  if (doGenMET){
+    for( GenMETCollection::const_iterator met = genmet->begin(); met != genmet->end() ; ++ met ) {
+      genMetPt.Fill(met->pt());
+    }
+  }
+
+  if (doL1Jets && doCaloJets && doGenJets)
+    L1Analysis(*caloJets,*genJets,*l1jets);
+
+  if ((errMsg != "") && (errCnt < errMax())){
+    errCnt=errCnt+1;
+    errMsg=errMsg + ".";
+    cout << "%L1JetPlots-Warning" << errMsg << endl;
+    if (errCnt == errMax()){
+      errMsg="%L1JetPLots-Warning -- Maximum error count reached -- No more messages will be printed.";
+      cout << errMsg << endl;
+    }
   }
 
 }
 
-void JetPlotsExample::endJob() {
+void L1JetPlots::L1Analysis(const reco::CaloJetCollection& caloJets,
+				const reco::GenJetCollection& genJets,
+				const l1extra::L1JetParticleCollection& l1Jets) {
+
+  //CalJetIter cIter;
+  //GenJetIter gIter;
+
+  //cout << "%doL1Analysis -- Number of l1jets:   " << l1Jets.size() << endl;
+  //cout << "%doL1Analysis -- Number of calojets: " << caloJets.size() << endl;
+
+
+  fillHist("L1JetCollSize",l1Jets.size());
+
+  for(l1extra::L1JetParticleCollection::const_iterator l1 = l1Jets.begin(); l1 != l1Jets.end(); ++l1) {
+    
+    Double_t pt_l1=l1->pt();
+    Double_t eta_l1=l1->eta();
+    Double_t phi_l1=l1->phi();
+    
+    fillHist("L1JetPt",pt_l1);
+    fillHist("L1JetPhi",phi_l1);
+    fillHist("L1JetEta",eta_l1);
+    
+    // match L1 Jets with Jets
+    mtchL1(eta_l1,phi_l1,pt_l1,caloJets,"_Calo");
+    mtchL1(eta_l1,phi_l1,pt_l1,genJets,"_Gen");
+  }
+}
+
+template <typename T> void L1JetPlots::mtchL1(const Double_t& eta_l1, 
+						  const Double_t& phi_l1, 
+						  const Double_t& pt_l1, 
+						  const T& jets,
+						  const TString& WhichJets) {
+
+  // kick out if Jet collection does not exist
+  if (! &jets) return;
+
+  double etaCut=2.5;
+  double drCut=0.5;
+
+
+  typedef typename T::const_iterator iter;
+  iter mIter;
+
+  float rmin(99.);
+  for ( iter jiter=jets.begin(); jiter!=jets.end(); jiter++) {
+    
+    if (fabs(jiter->eta()) < etaCut){
+
+      Double_t pt_jet=jiter->pt();
+      Double_t eta_jet=jiter->eta();
+      Double_t phi_jet=jiter->phi();
+      
+      float dr=deltaR(eta_jet,phi_jet,eta_l1,phi_l1);
+      if (pt_l1>20 && pt_jet>10) fillHist("L1DeltaR" + WhichJets,dr);
+      
+      if(dr<rmin){rmin=dr;mIter=jiter;}
+      
+    }  
+  }
+
+  if (rmin < drCut){
+    fill2DHist("L1etaVSJeteta" + WhichJets,mIter->eta(),eta_l1,1.);
+    fill2DHist("L1phiVSJetphi" + WhichJets,mIter->phi(),phi_l1,1.);
+    fill2DHist("L1ptVSJetpt" + WhichJets,mIter->pt(),pt_l1,1.);
+    
+    if (mIter->pt()>75.) {
+      fillHist("L1PtOverCaloPt" + WhichJets,pt_l1/(mIter->pt()));
+    }
+  }
+}
+
+void L1JetPlots::endJob() {
 
   //Write out the histogram file.
   m_file->Write();
 
 }
+
+void L1JetPlots::fillHist(const TString& histName, const Double_t& value, const Double_t& wt) {
+
+  hid=m_HistNames.find(histName);
+  if (hid==m_HistNames.end())
+    std::cout << "%fillHist -- Could not find histogram with name: " << histName << std::endl;
+  else
+    hid->second->Fill(value,wt); 
+
+}
+
+void L1JetPlots::fill2DHist(const TString& histName, const Double_t& x,const Double_t& y,const Double_t& wt) {
+
+  hid2D=m_HistNames2D.find(histName);
+  if (hid2D==m_HistNames2D.end())
+    std::cout << "%fillHist -- Could not find histogram with name: " << histName << std::endl;
+  else
+    hid2D->second->Fill(x,y,wt); 
+
+}
+
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE(JetPlotsExample);
+DEFINE_FWK_MODULE(L1JetPlots);
