7167bc659db2520b2f61851103e2be226526d573
apana <>
2012-06-12 22:31:11 +0000

    CompJets_cfi.py

--- MyHLT/HLTexamples/python/CompJets_cfi.py
+++ MyHLT/HLTexamples/python/CompJets_cfi.py
@@ -5,5 +5,6 @@ compJets = cms.EDAnalyzer("CompJets",
                        HLTriggerResults = cms.InputTag("TriggerResults::HLT"),
                        PFJetCollection1 = cms.InputTag("ak5PFJetsL1FastL2L3"),
                        PFJetCollection2 = cms.InputTag("hltAK5PFJetL1FastL2L3Corrected"),
-                       GenJetCollection = cms.InputTag("ak5GenJets")
+                       GenJetCollection = cms.InputTag("ak5GenJets"),
+                       Debug=cms.bool(False)
                        )
